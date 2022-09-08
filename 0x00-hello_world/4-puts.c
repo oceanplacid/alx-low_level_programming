218f37c0f6359714c2765cd424f6afef141d10d8
Hanif Dadzie <oceanplacid@gmail.com>
2022-09-08 16:11:47 -0700

    puts update2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 /*
- * puts output
+ *string printing function
+ * /
+/**
+ * main - prints a string to the screen
+ * Description: prints string
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+/**
+ * Return: 0 if executed right
+ */
 return (0);
 }
