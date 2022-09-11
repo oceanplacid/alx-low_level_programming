2ba3fb40959ebf9807f4d8fc5014a1ab85799017
Hanif Dadzie <oceanplacid@gmail.com>
2022-09-11 13:14:23 -0700

    5-printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints a string
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/* ruturn - returns 0 if there are no errors */
+	return (0);
+}
