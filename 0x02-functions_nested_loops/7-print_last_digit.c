38f546938dbd182bf96c016e9080ec0e43fc60c7
Hanif Dadzie <oceanplacid@gmail.com>
2022-12-14 08:24:36 -0800

    print last digit commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,7 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
-
-	last_digit = n % 10;
-	putchar (last_digit + '0');	
+	putchar (n%10 + '0');	
 	
-	return (last_digit);	
+	return (n%10);	
 }
