5e082a4dbda6317acf0cf8ab6b4be0b8ad8ea9b2
Hanif Dadzie <oceanplacid@gmail.com>
2022-12-14 08:59:03 -0800

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * Description - adds two integers
+ *
+ * main - check the code
+ *
+ *Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 char _putchar(char);
 int print_last_digit(int);
+int add(int, int);
 void print_to_98(int n);
