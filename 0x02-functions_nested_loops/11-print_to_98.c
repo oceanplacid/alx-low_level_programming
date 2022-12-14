59a3ad1ffd04fc1af5206696091caef71e5fb88d
Hanif Dadzie <oceanplacid@gmail.com>
2022-12-14 05:43:00 -0800

    print to 98 commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@
 void print_to_98(int n)
 {
 	int x;
-	
+
 	if (n >= 0)
 	{
 		for (x = n; x <= 98; x++)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
+void print_to_98(int n);
