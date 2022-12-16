1def0affec96ce699846ceda80a9c6502518a084
Hanif Dadzie <oceanplacid@gmail.com>
2022-12-16 03:19:18 -0800

    isuuper update commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _isupper - checks if an input is an upper case or not
+ * Return: 1 if it is upper case, and 0 if it is not
+ * @c: parameter passed to the _issupper function
+ */
 int _isupper(int c)
 {
 	if (isupper(c))
