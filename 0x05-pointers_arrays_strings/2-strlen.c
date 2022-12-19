9922c7bb6178c201458f8d8c089da868f7351e73
Hanif Dadzie <oceanplacid@gmail.com>
2022-12-19 11:59:13 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * Return: char
+ * @s: parameter passed to the function
+ */
+int _strlen(char *s)
+{
+	unsigned long int a;
+
+	a = sizeof(s);
+	return (a);
+}
